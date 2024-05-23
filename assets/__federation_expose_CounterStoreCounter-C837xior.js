2a41c216d572d4b928551df0520d6f0d601a4a13
dmifsud <dmifsud@users.noreply.github.com>
2024-05-23 02:33:52 +0000

    deploy: cae24f07f7266a265bf0284574e7dc4a6de558e8

---
+++ assets/__federation_expose_CounterStoreCounter-C837xior.js
@@ -0,0 +1,10 @@
+import { importShared } from './__federation_fn_import-BSSw6lBa.js';
+
+const {create} = await importShared('zustand');
+
+const useStore = create((set) => ({
+  count: 1,
+  inc: (amount) => set((state) => ({ count: state.count + (amount ?? 2) }))
+}));
+
+export { useStore as default };
